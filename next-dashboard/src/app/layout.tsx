6c88d1c9c219d8f99a5a811878df0997d3900412
YasinDev <yasinnasiry18@gmail.com>
2026-02-09 12:50:54 +0330

    ✨ feat: add ChakraProvider to layout for UI theming support
    This enables the use of Chakra UI components throughout the application by wrapping the app with the necessary provider. The "use client" directive is added because ChakraProvider is a client component.

--- next-dashboard/src/app/layout.tsx
+++ next-dashboard/src/app/layout.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -25,7 +28,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <ChakraProvider value={defaultSystem}>
+          {children}
+        </ChakraProvider>
       </body>
     </html>
   );
